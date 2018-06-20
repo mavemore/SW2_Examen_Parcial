11a03e3e3d38a5c61dd6ef49a3e6c12fb8db8dc1
Jose Moreira <jmoreira_20@hotmail.com>
2018-06-20 12:42:49 -0500

    tema

---
+++ test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import unittest
+import cotizador
+
+class Test(unittest.TestCase):
+	#Reemplace ID por el número identificador que utilizó en la plantilla de pruebas.
+	def test_cotizador_ID(self):
+		self.assertEqual("","")
+
+if __name__ == '__main__':
+	unittest.main()
