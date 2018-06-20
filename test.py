2071623f9526904757d9b7351b9024f5d40fbb03
Galo Castillo <gadacast@espol.edu.ec>
2018-06-20 15:35:31 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -6,82 +6,76 @@ import cotizador
 
 class Test(unittest.TestCase):
 	#Reemplace ID por el número identificador que utilizó en la plantilla de pruebas.
-	def test_cotizador_ID1(self):
+	def test_cotizador_cp1(self):
+		edad = 20
+		ciudad = 'Guayaquil'
+		sexo = 'hombre'
+		estado_civil = 'soltero'
+		especial = 'osteoporosis'
 		dependientes = 0
-		ciudad = "Guayaquil"
-		edad = 25
-		sexo = "mujer"
-		estado_civil = "casado"
+		cotizacion = cotizador.cotizar_seguro(ciudad, edad, sexo, estado_civil, especial, dependientes)
+		self.assertEqual(cotizacion , 'El valor calculado de su cotización es de 70.00')
+
+	def test_cotizador_cp2(self):
+		edad = 20
+		ciudad = 'Quito'
+		sexo = 'mujer'
+		estado_civil = 'casado'
+		especial = 'infarto'
 		dependientes = 1
-		especial = ""
-		cuota = cotizador.cotizar_seguro(ciudad, edad, sexo, estado_civil, especial, dependientes)
-		self.assertEqual(cuota,"El valor calculado de su cotización es de 80.00")
+		cotizacion = cotizador.cotizar_seguro(ciudad, edad, sexo, estado_civil, especial, dependientes)
+		self.assertEqual(cotizacion , 'El valor calculado de su cotización es de 80.00')
 
-	def test_cotizador_ID2(self):
-		dependientes = 0
-		ciudad = "Guayaquil"
-		edad = 25
-		sexo = "mujer"
-		estado_civil = "soltero"
-		dependientes = 1
-		especial = ""
-		cuota = cotizador.cotizar_seguro(ciudad, edad, sexo, estado_civil, especial, dependientes)
-		self.assertEqual(cuota,"El valor calculado de su cotización es de 70.00")
+	def test_cotizador_cp3(self):
+		edad = 20
+		ciudad = 'Cuenca'
+		sexo = 'mujer'
+		estado_civil = 'divorciado'
+		especial = 'diabetes'
+		dependientes = 2
+		cotizacion = cotizador.cotizar_seguro(ciudad, edad, sexo, estado_civil, especial, dependientes)
+		self.assertEqual(cotizacion , 'El valor calculado de su cotización es de 100.00')
 
-	def test_cotizador_ID3(self):
-		dependientes = 0
-		ciudad = "Guayaquil"
-		edad = 25
-		sexo = "hombre"
-		estado_civil = "soltero"
-		dependientes = 1
-		especial = ""
-		cuota = cotizador.cotizar_seguro(ciudad, edad, sexo, estado_civil, especial, dependientes)
-		self.assertEqual(cuota,"El valor calculado de su cotización es de 100.00")
+	def test_cotizador_cp4(self):
+		edad = 20
+		ciudad = 'Machala'
+		sexo = 'mujer'
+		estado_civil = 'divorciado'
+		especial = 'cancer'
+		dependientes = 3
+		cotizacion = cotizador.cotizar_seguro(ciudad, edad, sexo, estado_civil, especial, dependientes)
+		self.assertEqual(cotizacion , 'El valor calculado de su cotización es de 110.00')
 
-	def test_calcular_cuota_basica1(self):
-		cuota = cotizador.calcular_cuota_basica(0)
-		self.assertEqual(cuota,30)
+	def test_cotizador_cp5(self):
+		edad = 20
+		ciudad = 'Cuenca'
+		sexo = 'mujer'
+		estado_civil = 'divorciado'
+		especial = 'cancer'
+		dependientes = 4
+		cotizacion = cotizador.cotizar_seguro(ciudad, edad, sexo, estado_civil, especial, dependientes)
+		self.assertEqual(cotizacion , 'El valor calculado de su cotización es de 130.00')
+	
 
-	def test_calcular_cuota_basica2(self):
-		cuota = cotizador.calcular_cuota_basica(1)
-		self.assertEqual(cuota,60)
+	def test_cotizador_cp6(self):
+		edad = 20
+		ciudad = 'Cuenca'
+		sexo = 'XYZ'
+		estado_civil = 'divorciado'
+		especial = 'cancer'
+		dependientes = 4
+		cotizacion = cotizador.cotizar_seguro(ciudad, edad, sexo, estado_civil, especial, dependientes)
+		self.assertEqual(cotizacion , 'El valor calculado de su cotización es de 40.00')
 
-	def test_calcular_cuota_basica3(self):
-		cuota = cotizador.calcular_cuota_basica(2)
-		self.assertEqual(cuota,90)
-
-	def test_calcular_cuota_basica3(self):
-		cuota = cotizador.calcular_cuota_basica(3)
-		self.assertEqual(cuota,100)
-
-	def test_calcular_cuota_basica3(self):
-		cuota = cotizador.calcular_cuota_basica(4)
-		self.assertEqual(cuota,120)
-
-	def test_calcular_valores_adicionales1(self):
-		calcular = cotizador.calcular_valores_adicionales(18, "hombre", "casado", "")
-		self.assertEqual(calcular,0)
-
-	def test_calcular_valores_adicionales2(self):
-		calcular = cotizador.calcular_valores_adicionales(18, "mujer", "soltero", "")
-		self.assertEqual(calcular,10)
-
-	def test_calcular_valores_adicionales3(self):
-		calcular = cotizador.calcular_valores_adicionales(40, "mujer", "soltero", "osteoporosis")
-		self.assertEqual(calcular,10)
-
-	def test_calcular_valores_adicionales4(self):
-		calcular = cotizador.calcular_valores_adicionales(41, "hombre", "soltero", "infarto")
-		self.assertEqual(calcular,50)
-
-	def test_calcular_valores_adicionales5(self):
-		calcular = cotizador.calcular_valores_adicionales(61, "hombre", "soltero", "cancer")
-		self.assertEqual(calcular,50)
-
-	def test_calcular_valores_adicionales6(self):
-		calcular = cotizador.calcular_valores_adicionales(61, "mujer", "soltero", "cancer")
-		self.assertEqual(calcular,60)
+	def test_cotizador_cp7(self):
+		edad = 20
+		ciudad = 'Cuenca'
+		sexo = 'mujer'
+		estado_civil = 'ABC'
+		especial = 'cancer'
+		dependientes = 4
+		cotizacion = cotizador.cotizar_seguro(ciudad, edad, sexo, estado_civil, especial, dependientes)
+		self.assertEqual(cotizacion , 'El valor calculado de su cotización es de 40.00')
 
 if __name__ == '__main__':
 	unittest.main()
