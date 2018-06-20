a2692d99cef048612f73d1aefb96f29d49ddcc2c
Arelys Vintimilla <aremvint@espol.edu.ec>
2018-06-20 15:31:48 -0500

    examen segundo parcial

--- test.py
+++ test.py
@@ -5,9 +5,30 @@ import unittest
 import cotizador
 
 class Test(unittest.TestCase):
-	#Reemplace ID por el número identificador que utilizó en la plantilla de pruebas.
-	def test_cotizador_ID(self):
-		self.assertEqual("","")
+    def test_cotizador_1(self)
+        self.assertEqual("El valor es de 60.00",cotizador.cotizar_seguro("Cuenca",35, "hombre", "casado", "",0))
+
+    def test_cotizador_2(self)
+        self.assertEqual("El valor es de 70.00",cotizador.cotizar_seguro("Cuenca",35, "hombre", "soltero", "",0))	
+
+    def test_cotizador_3(self)
+        self.assertEqual("El valor es de 0.00",cotizador.cotizar_seguro("Cuenca",16, "hombre", "soltero", "",0))	
+
+    def test_cotizador_4(self)
+        self.assertEqual("El valor es de 80.00",cotizador.cotizar_seguro("Guayaquil",18, "mujer", "casado", "",0))
+
+    def test_cotizador_5(self)
+        self.assertEqual("El valor es de 40.00",cotizador.cotizar_seguro("Guayaquil",18, "mujer", "soltero", "",0))	
+
+    def test_cotizador_6(self)
+        self.assertEqual("El valor es de 0.00",cotizador.cotizar_seguro("Guayaquil",16, "mujer", "soltero", "",0))
+
+    def test_cotizador_7(self)
+        self.assertEqual("El valor es de 130.00",cotizador.cotizar_seguro("Cuenca",35, "hombre", "soltero", "",2))
+
+    def test_cotizador_8(self)
+        self.assertEqual("El valor es de 120.00",cotizador.cotizar_seguro("Guayaquil",18, "mujer", "casado", "",3))		
+	
 
 if __name__ == '__main__':
 	unittest.main()
