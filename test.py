96546bd42007c5a6d0544b4011c71271ecc54a29
Cliente CONTROL-PC <CltControl@GGIECSOFWRK137.espol.edu.ec>
2018-06-20 14:46:33 -0500

    Examen

--- test.py
+++ test.py
@@ -9,5 +9,28 @@ class Test(unittest.TestCase):
 	def test_cotizador_ID(self):
 		self.assertEqual("","")
 
+	def test1(self):
+		res = cotizador.cotizar_seguro('Guayaquil', 20, 'mujer', 'casado', '', 2)
+		self.assertEqual("El valor calculado de su cotización es de 110.00", res)
+	def test2(self):
+		res = cotizador.cotizar_seguro('Quito', 35, 'hombre', 'soltero', '', 0)
+		self.assertEqual("El valor calculado de su cotización es de 70.00", res)
+	def test1(self):
+		res = cotizador.cotizar_seguro('Cuenca', 18, 'mujer', 'soltero', '', 0)
+		self.assertEqual("El valor calculado de su cotización es de 40.00", res)
+	def test1(self):
+		res = cotizador.cotizar_seguro('Guayaquil', 25, 'hombre', 'casado', '', 1)
+		self.assertEqual("El valor calculado de su cotización es de 60.00", res)
+	def test1(self):
+		res = cotizador.cotizar_seguro('Cuenca', 30, 'hombre', 'casado', '', 4)
+		self.assertEqual("El valor calculado de su cotización es de 120.00", res)
+	def test1(self):
+		res = cotizador.cotizar_seguro('Machala', 35, 'mujer', 'viuda', '', 2)
+		self.assertEqual("El valor calculado de su cotización es de 100.00", res)
+
+
+
+
 if __name__ == '__main__':
 	unittest.main()
+
